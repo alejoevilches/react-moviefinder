adf9ae0c3dee536b2bd1d73002c05f5f61019e49
alejoevilches <alejoevilches@gmail.com>
2023-07-15 16:40:05 -0300

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='page'>
+      <h1>Buscador de peliculas</h1>
+      <header>
+        <form className='form'>
+          <label htmlFor="movie-name">¿Que pelicula querés buscar?</label>
+          <input type="text" id="movie-name" placeholder='Licorice Pizza, Everything Everywhere All At Once...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+       Aca van los resultados
+      </main>
+    </div>
   )
 }
 
