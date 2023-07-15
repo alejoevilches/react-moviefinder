26966f9b711406d787396670b34cf0b34a15ad43
alejoevilches <alejoevilches@gmail.com>
2023-07-15 17:14:48 -0300

    add render movies on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
+import withResults from "./mocks/with-results.json"
+import withoutResults from "./mocks/no-results.json"
+
 
 function App() {
+  const movies=withResults.Search;
+  const hasMovies=movies?.length > 0
   return (
     <div className='page'>
       <h1>Buscador de peliculas</h1>
@@ -12,7 +17,19 @@ function App() {
         </form>
       </header>
       <main>
-       Aca van los resultados
+       {
+        hasMovies ? (
+          movies.map((movie)=>{
+            return (<li key={movie}>
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <img src={movie.Poster} alt={movie.Title} />
+            </li>)
+          })
+        ) : (
+          <h1>No se encontraron resultados. Proba buscar nuevamente</h1>
+        )
+       }
       </main>
     </div>
   )
