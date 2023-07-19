676e9e7c8b7fa664ef5d5b7c1edf814266dda101
alejoevilches <alejoevilches@gmail.com>
2023-07-19 18:56:33 -0300

    add useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './App.css'
+import { useRef } from 'react';
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies';
 
 function App() {
   const {movies:mappedMovies}=useMovies();
-  console.log(useMovies())
+  const inputRef=useRef();
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const value=inputRef.current.value;
+    alert(value);
+  }
   return (
     <div className='page'>
       <h1>Buscador de peliculas</h1>
       <header>
         <form className='form'>
           <label htmlFor="movie-name">¿Que pelicula querés buscar?</label>
-          <input type="text" id="movie-name" placeholder='Licorice Pizza, Everything Everywhere All At Once...' />
-          <button type='submit'>Buscar</button>
+          <input ref={inputRef}type="text" id="movie-name" placeholder='Licorice Pizza, Everything Everywhere All At Once...' />
+          <button onClick={handleSubmit} type='submit'>Buscar</button>
         </form>
       </header>
       <main>
