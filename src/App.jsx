919567fd94279363833ecba352039b8d726ab681
alejoevilches <alejoevilches@gmail.com>
2023-07-27 13:56:44 -0300

    fix useRef use

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import './App.css'
-import { useRef } from 'react';
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies';
 
 function App() {
   const {movies:mappedMovies}=useMovies();
-  const inputRef=useRef();
   const handleSubmit=(e)=>{
     e.preventDefault();
-    const value=inputRef.current.value;
-    alert(value);
+    const data=new FormData(e.target);
+    const query=data.get("query");
   }
   return (
     <div className='page'>
       <h1>Buscador de peliculas</h1>
       <header>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
           <label htmlFor="movie-name">¿Que pelicula querés buscar?</label>
-          <input ref={inputRef}type="text" id="movie-name" placeholder='Licorice Pizza, Everything Everywhere All At Once...' />
-          <button onClick={handleSubmit} type='submit'>Buscar</button>
+          <input name="query" type="text" id="movie-name" placeholder='Licorice Pizza, Everything Everywhere All At Once...' />
+          <button type='submit'>Buscar</button>
         </form>
       </header>
       <main>
